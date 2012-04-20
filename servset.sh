4c2cf7938e212eabb12e65e0070fda6d8a2cf55f
Wezegege <wezegege@live.fr>
2012-04-20 09:57:53 +0200

    sagemcom config

---
+++ servset.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+export https_proxy=http://10.66.243.130:8080
+git clone https://github.com/wezegege/config.git config
+mv .bashrc .bashrc.old
+ln -s config/bashrc .bashrc
+ln -s config/vimrc .vimrc
+ln -s config/vim .vim
+ln -s config/inputrc .inputrc
+ln -s config/commonrc .commonrc
+ln -s config/gitconfig .gitconfig
+cd config
+git submodule update --init
+cd ~
+mkdir -p .local/share/vim
+mkdir .ssh
+mv authorized_keys .ssh
+
+source ~/.bashrc

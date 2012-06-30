3ad27eff085f41aca06417d958af4ffdba3a2988
Wezegege <wezegege@live.fr>
2012-06-30 12:55:08 +0200

    fab utils update

--- scripts/fabfile/__init__.py
+++ scripts/fabfile/__init__.py
@@ -13,6 +13,8 @@ import ldapsync
 import db
 
 env.use_ssh_config = True
+env.colors = True
+env.format = True
 
 env.roledefs = {
     'main' : ['biblio', 'etl', 'demo'],
@@ -41,24 +43,43 @@ def config():
   prompts += expect('Password', password)
   with cd('~/config'):
     with prefix('export https_proxy=http://10.66.243.130:8080/'):
-      #with hide('stdout'):
-      with expecting(prompts):
-        erun('git pull')
+      with hide('stdout'):
+        with expecting(prompts):
+          erun('git pull')
+      run('git submodule update --init')
 
 @task
 def create_user(alias):
   key_dir = '~/.ssh/keys'
   config_file = '~/.ssh/config'
   user = 'kevin'
-  sudo('adduser %s' % user)
+  sudo('adduser {user}')
   sudo('visudo')
   with settings(warn_only=True):
-    if not run('git --version'):
+    if run('git --version').failed:
       sudo('apt-get install git-core')
   local('ssh-keygen -f %s' % (os.path.join(key_dir), alias))
-  local('echo "\\nHost %s\\n    Hostname %s\\n    User %s\\n    IdentityFile %s" >> %s' %
-      (alias, env.host_string, user, os.path.join(key_dir, alias), config_file))
-  put('%s/%s.pub' % (key_dir, alias), '/home/%s/authorized_keys' % user)
-  put('~/config/scripts/servset.sh', '/home/%s' % user)
-  run('/home/%s/servset.sh' % user)
+  local("""echo "
+Host {alias}
+  Hostname {env.host_string}
+  User {user}
+  IdentityFile {key_dir}/{alias}
+" >> {config_file}""")
+  run('mkdir /home/{user}/.ssh')
+  put('{key_dir}/{alias}.pub', '/home/{user}/.ssh/authorized_keys')
+  password = ask_password()
+  prompts = list()
+  prompts += expect('Password', password)
+  with cd('~'):
+    with prefix('export https_proxy=http://10.66.243.130:8080/'):
+      with settings(
+          hide('stdout'),
+          expecting(prompts)):
+        erun('git clone https://wezegege@bitbucket.org/wezegege/config.git')
+      run('git submodule update --init', dir='~/config')
+    for conf in ('bashrc', 'vimrc', 'vim', 'inputrc', 'commonrc', 'gitconfig'):
+      with settings(warn_only=True):
+        if run('ls .{conf}').failed:
+          run('mv .{conf} .{conf}.old')
+      run('ln -s config/{conf} .{conf}')
 

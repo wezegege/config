d5db75842258d66d2bcde9f6e43df1f2990a0234
Wezegege <wezegege@live.fr>
2012-11-28 10:44:27 +0100

    subversion config

--- scripts/userset.sh
+++ scripts/userset.sh
@@ -3,8 +3,7 @@ export https_proxy=http://10.66.243.130:8080
 git clone https://wezegege@bitbucket.org/wezegege/config.git
 cd ~/config
 git submodule update --init
-mv .bashrc .bashrc.old
-mkdir ~/.ssh ~/.config/terminator
+mkdir ~/.ssh ~/.config/terminator ~/.subversion
 ln -sf ~/config/zshrc ~/.zshrc
 ln -sf ~/config/bashrc ~/.bashrc
 ln -sf ~/config/vimrc ~/.vimrc
@@ -14,5 +13,8 @@ ln -sf ~/config/commonrc ~/.commonrc
 ln -sf ~/config/gitconfig ~/.gitconfig
 ln -sf ~/config/config-terminator-config ~/.config/terminator/config
 ln -sf ~/config/ssh-config ~/.ssh/config
+ln -sf ~/config/subversion-config ~/.subversion/config
+ln -sf ~/config/subversion-servers ~/.subversion/servers
+ln -sf ~/config/uncommonrc/sagemcom .uncommonrc
 
 source ~/.bashrc

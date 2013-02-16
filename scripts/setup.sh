9a1e6a72309c788635a87b08a1da9f1586053b82
Wezegege <wezegege@live.fr>
2013-02-16 21:25:05 +0100

    setup considers uncommon

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 # -*- coding: utf-8 -*-
 
+function put_link {
+    link=$1
+    dest=$2
+    [[ -e $(dirname ${dest}) ]] || mkdir -p $(dirname ${dest})
+    if [[ -e ${dest} && $(readlink ${dest}) == ${config_folder}/${config_file} ]] ; then
+        ln -sf ${config_folder}/${config_file} ${dest}
+    else
+        ln -sbf ${config_folder}/${config_file} ${dest}
+    fi
+
+}
+
 config_folder="${HOME}/config/config"
 for config_file in $(ls ${config_folder}); do
     path="${HOME}/.$(echo ${config_file} | tr '-' '/')"
-    [[ -e $(dirname ${path}) ]] || mkdir -p $(dirname ${path})
-    if [[ -e ${path} && $(readlink ${path}) == ${config_folder}/${config_file} ]] ; then
-        ln -sf ${config_folder}/${config_file} ${path}
-    else
-        ln -sbf ${config_folder}/${config_file} ${path}
-    fi
+    put_link ${config_folder}/${config_file} ${path}
 done
+
+if [[ $# >= 1 ]] ; then
+    uncommonrc=${HOME}/config/uncommonrc/$1
+    put_link ${uncommonrc} ${HOME}/.uncommonrc
+fi
